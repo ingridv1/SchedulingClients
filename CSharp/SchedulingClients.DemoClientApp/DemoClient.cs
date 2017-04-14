841c9baaac46265682111d34390352a3ab236855
Dr Martin Davies <martin.davies@guidanceautomation.com>
2017-04-14 10:49:02 +0100

    Adds basic logging framework
    Logging is now configured.

--- CSharp/SchedulingClients.DemoClientApp/DemoClient.cs
+++ CSharp/SchedulingClients.DemoClientApp/DemoClient.cs
@@ -2,14 +2,21 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
+using NLog;
+using System.IO;
+using NLog.Targets;
+using NLog.Config;
 
 namespace SchedulingClients.DemoClientApp
 {
     public class DemoClient : IDisposable
     {
+        private readonly string logFolderPath;
+
         private ObservableCollection<IClient> clients = new ObservableCollection<IClient>();
 
         private EndpointSettings endpointSettings;
+
         private bool isDisposed = false;
 
         private ReadOnlyObservableCollection<IClient> readOnlyClients;
@@ -17,8 +24,10 @@ namespace SchedulingClients.DemoClientApp
         public DemoClient(IPAddress ipAddress, int httpPort, int tcpPort)
         {
             this.endpointSettings = new EndpointSettings(ipAddress, httpPort, tcpPort);
-
             readOnlyClients = new ReadOnlyObservableCollection<IClient>(clients);
+
+            logFolderPath = Path.Combine(Path.GetTempPath(), "Scheduling Clients Demo Client App");
+            Directory.CreateDirectory(logFolderPath);
 
             Configure();
         }
@@ -35,6 +44,8 @@ namespace SchedulingClients.DemoClientApp
         public JobBuilderClient JobBuilderClient { get { return (JobBuilderClient)clients.FirstOrDefault(e => e is JobBuilderClient); } }
 
         public JobsStateClient JobsStateClient { get { return (JobsStateClient)clients.FirstOrDefault(e => e is JobsStateClient); } }
+
+        public string LogFolderPath { get { return logFolderPath; } }
 
         public RoadmapClient RoadmapClient { get { return (RoadmapClient)clients.FirstOrDefault(e => e is RoadmapClient); } }
 
@@ -53,6 +64,13 @@ namespace SchedulingClients.DemoClientApp
 
             ServicingClient servicingClient = new ServicingClient(endpointSettings.TcpServicingService());
             clients.Add(servicingClient);
+
+            LogManager.Configuration = new LoggingConfiguration();
+
+            foreach (IClient client in clients)
+            {
+                client.Logger = GetLogger(client);
+            }
         }
 
         public void Dispose()
@@ -74,6 +92,23 @@ namespace SchedulingClients.DemoClientApp
             }
 
             isDisposed = true;
+        }
+
+        private Logger GetLogger(IClient client)
+        {
+            string name = client.GetType().Name;
+
+            FileTarget target = new FileTarget(name);
+            target.Layout = @"${processtime} ${level: padding = -8:padCharacter = } ${message}";
+            target.FileName = Path.Combine(new[] { logFolderPath, name + ".log" });
+
+            LoggingRule rule = new LoggingRule(name, LogLevel.Trace, target);
+            LogManager.Configuration.LoggingRules.Add(rule);
+            LogManager.Configuration.AddTarget(target);
+
+            LogManager.ReconfigExistingLoggers();
+
+            return LogManager.GetLogger(name);
         }
     }
 }
