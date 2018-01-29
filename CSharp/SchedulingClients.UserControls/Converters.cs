bb6db80670f0030c08c6c4aacacb99128c3c1ddc
Dr Martin Davies <Martin.davies@guidanceautomation.com>
2018-01-29 14:24:59 +0000

    Updated to UDP Casts 3.1.0

--- CSharp/SchedulingClients.UserControls/Converters.cs
+++ CSharp/SchedulingClients.UserControls/Converters.cs
@@ -3,10 +3,25 @@ using System;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using UDPCasts;
 using System.Windows.Data;
 
 namespace SchedulingClients.UserControls
 {
+    public class DegStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            PoseIPStruct poseIP = (PoseIPStruct)value;
+            return poseIP.ToPoseIPStringDeg();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class ServiceTypeArrayToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
