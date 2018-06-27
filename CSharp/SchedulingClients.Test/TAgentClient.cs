eaa52161762cc914f181b40aeb55df0b083bcd92
Callum Clarke <callum.clarke@automation1.com>
2018-06-27 15:34:47 +0100

    Adds Test Project
    Adds initial test project including mocking and unit testing stuff for demonstration.

---
+++ CSharp/SchedulingClients.Test/TAgentClient.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchedulingClients.AgentServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GAClients;
+
+namespace SchedulingClients.Test
+{
+	[TestFixture]
+	class TAgentClient
+	{
+		public bool VerifyAgents(IEnumerable<AgentData> agents)
+		{
+			if (agents.Any())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		[Test]
+		public void CanGetAgentData()
+		{
+			//arrange
+			//CustomData
+			AgentData agentData = new AgentData();
+			List<AgentData> agentDatas = new List<AgentData>();
+			agentDatas.Add(agentData);
+			IEnumerable<AgentData> returnedAgentData = agentDatas as IEnumerable<AgentData>;
+			ServiceOperationResult result = new ServiceOperationResult(0, "Success", null, null);
+			//Mock
+			Mock<IAgentClient> mock = new Mock<IAgentClient>();
+			mock.Setup(e => e.TryGetAllAgentData(out returnedAgentData)).Returns(result);
+
+			//act
+			IEnumerable<AgentData> myAgentData;
+			ServiceOperationResult opResult = mock.Object.TryGetAllAgentData(out myAgentData);
+			bool finalResult = VerifyAgents(myAgentData);
+
+			//assert
+			Assert.That(opResult.ClientException == null && opResult.ServiceException == null && finalResult);
+		}
+	}
+}
