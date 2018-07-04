1ece51c1bb6474005563d2c483ab8b9ee18d17c0
rhys howard <rhys.howard@automation1.com>
2018-07-04 14:10:37 +0100

    Added job creation spammer

--- CSharp/SchedulingClients.UserControls/Spammer.xaml.cs
+++ CSharp/SchedulingClients.UserControls/Spammer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SchedulingClients.MapServiceReference;
+﻿using SchedulingClients.JobBuilderServiceReference;
+using SchedulingClients.MapServiceReference;
 using Sparrow.Chart;
 using System;
 using System.Collections.Generic;
@@ -22,33 +23,92 @@ namespace SchedulingClients.UserControls
     /// </summary>
     public partial class Spammer : UserControl
     {
+        private MapClient mapClient = null;
+
         public Spammer()
         {
             InitializeComponent();
         }
 
-        private void spamButton_Click(object sender, RoutedEventArgs e)
+        public void Configure(MapClient mapClient)
         {
-            MapClient mapClient = DataContext as MapClient;
+            this.mapClient = mapClient;
+        }
 
-            rttLineSeries.Points.Clear();
-
-            Task.Factory.StartNew(() =>
+        private void spamMapButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (mapClient != null)
             {
-                for (int i = 0; i < 100; i++)
+                rttLineSeries.Points.Clear();
+
+                Task.Factory.StartNew(() =>
                 {
-                    IEnumerable<NodeData> mock;
-                    DateTime before = DateTime.Now;
-                    mapClient.TryGetAllNodeData(out mock);
-                    TimeSpan rtt = DateTime.Now.Subtract(before);
-                    Application.Current.Dispatcher.BeginInvoke((Action) (() =>
+                    for (int i = 0; i < 100; i++)
                     {
-                        spamIndicator.IsChecked = !spamIndicator.IsChecked;
-                        rttText.Text = rtt.Milliseconds.ToString();
-                        rttLineSeries.Points.Add(new DoublePoint() { Data = i, Value = rtt.Milliseconds });
-                    }));
-                }
-            });
+                        IEnumerable<NodeData> mock;
+                        DateTime before = DateTime.Now;
+                        mapClient.TryGetAllNodeData(out mock);
+                        TimeSpan rtt = DateTime.Now.Subtract(before);
+                        Application.Current.Dispatcher.BeginInvoke((Action)(() =>
+                   {
+                            spamIndicator.IsChecked = !spamIndicator.IsChecked;
+                            rttText.Text = rtt.Milliseconds.ToString();
+                            rttLineSeries.Points.Add(new DoublePoint() { Data = i, Value = rtt.Milliseconds });
+                        }));
+                    }
+                });
+            }
+            else
+            {
+                MessageBox.Show("Map client is null", "No Map Client", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void spamJobButton_Click(object sender, RoutedEventArgs e)
+        {
+            JobBuilderClient jobBuilderClient = DataContext as JobBuilderClient;
+
+            if (mapClient != null)
+            {
+                rttLineSeries.Points.Clear();
+
+                Task.Factory.StartNew(() =>
+                {
+                    Random random = new Random();
+                    for (int i = 0; i < 100; i++)
+                    {
+                        DateTime before = DateTime.Now;
+                        JobData jobData;
+                        jobBuilderClient.TryCreateJob(out jobData);
+                        IEnumerable<NodeData> nodeDataEnumerable;
+                        mapClient.TryGetAllNodeData(out nodeDataEnumerable);
+                        int sleepingTaskId1;
+                        jobBuilderClient.TryCreateSleepingTask(jobData.OrderedListTaskId, nodeDataEnumerable.ElementAt(random.Next(nodeDataEnumerable.Count())).MapItemId, out sleepingTaskId1);
+                        int sleepingTaskId2;
+                        jobBuilderClient.TryCreateSleepingTask(jobData.OrderedListTaskId, nodeDataEnumerable.ElementAt(random.Next(nodeDataEnumerable.Count())).MapItemId, out sleepingTaskId2);
+                        bool success;
+                        jobBuilderClient.TryCommit(jobData.JobId, out success);
+                        TimeSpan rtt = DateTime.Now.Subtract(before);
+                        Application.Current.Dispatcher.BeginInvoke((Action)(() =>
+                        {
+                            if (!success || jobData.JobId < 0 || sleepingTaskId1 < 0 || sleepingTaskId2 < 0)
+                            {
+                                MessageBox.Show("Job creation failed");
+                            }
+                            else
+                            {
+                                spamIndicator.IsChecked = !spamIndicator.IsChecked;
+                                rttText.Text = rtt.Milliseconds.ToString();
+                                rttLineSeries.Points.Add(new DoublePoint() { Data = i, Value = rtt.Milliseconds });
+                            }
+                        }));
+                    }
+                });
+            }
+            else
+            {
+                MessageBox.Show("Map client is null", "No Map Client", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
