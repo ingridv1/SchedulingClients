602846d80c6e9009d3b153c1b647b69f37e33a17
Martin Davies <martin.davies@guidanceautomation.com>
2018-08-17 07:56:18 +0100

    Baby steps
    Going down the 2nd solution approach

---
+++ CSharp/SchedulingClientsl/Client Interfaces/IJobsStateClient.cs
@@ -0,0 +1,26 @@
+﻿//using GAClients;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SchedulingClients
+//{
+//	public interface IJobsStateClient : ICallbackClient
+//	{
+//		ServiceOperationResult TryAbortAllJobs(out bool success);
+
+//		ServiceOperationResult TryAbortJob(int jobId, out bool success);
+
+//		ServiceOperationResult TryAbortTask(int taskId, out bool success);
+
+//		ServiceOperationResult TryGetActiveJobIdsForAgent(int agentId, out IEnumerable<int> jobIds);
+
+//		ServiceOperationResult TryResolveFaultedTask(int taskId, out bool success);
+
+//		ServiceOperationResult TryResolveFaultedJob(int jobId, out bool success);
+
+
+//	}
+//}
