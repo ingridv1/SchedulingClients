24f39aad255935637667c2493670894b2cb45b6d
Martin Davies <martin.davies@guidanceautomation.com>
2018-08-17 17:51:52 +0100

    Adds agent statecast client

--- CSharp/SchedulingClients/Client Interfaces/IAgentStatecastClient.cs
+++ CSharp/SchedulingClients/Client Interfaces/IAgentStatecastClient.cs
@@ -1,30 +1,30 @@
-﻿//using GAClients;
-//using SchedulingClients.AgentStateCastServiceReference;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using BaseClients;
+using SchedulingClients.AgentStatecastServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace SchedulingClients
-//{
-//	public interface IAgentStatecastClient : IClient
-//	{
-//		ServiceOperationResult TryGetEnumStatecastValue(int agentId, string parameterAlias, out byte parameterValue);
+namespace SchedulingClients
+{
+    public interface IAgentStatecastClient : IClient
+    {
+        ServiceOperationResult TryGetEnumStatecastValue(int agentId, string parameterAlias, out byte parameterValue);
 
-//		ServiceOperationResult TryGetFloatStatecastValue(int agentId, string parameterAlias, out float parameterValue);
+        ServiceOperationResult TryGetFloatStatecastValue(int agentId, string parameterAlias, out float parameterValue);
 
-//		ServiceOperationResult TryGetShortStatecastValue(int agentId, string parameterAlias, out short parameterValue);
+        ServiceOperationResult TryGetShortStatecastValue(int agentId, string parameterAlias, out short parameterValue);
 
-//		ServiceOperationResult TryGetUShortStatecastValue(int agentId, string parameterAlias, out ushort parameterValue);
+        ServiceOperationResult TryGetUShortStatecastValue(int agentId, string parameterAlias, out ushort parameterValue);
 
-//        ServiceOperationResult TryGetUIntegerStatecastValue(int agentId, string parameterAlias, out uint parameterValue);
+        ServiceOperationResult TryGetUIntegerStatecastValue(int agentId, string parameterAlias, out uint parameterValue);
 
-//        ServiceOperationResult TryGetIntegerStatecastValue(int agentId, string parameterAlias, out int parameterValue);
+        ServiceOperationResult TryGetIntegerStatecastValue(int agentId, string parameterAlias, out int parameterValue);
 
-//        ServiceOperationResult TryGetIPAddressStatecastValue(int agentId, string parameterAlias, out IPAddress parameterValue);
+        ServiceOperationResult TryGetIPAddressStatecastValue(int agentId, string parameterAlias, out IPAddress parameterValue);
 
-//		ServiceOperationResult TryGetStatecastDescription(int agentId, out CastType statecastDescription);
-//	}
-//}
+        ServiceOperationResult TryGetStateCastVariableDefinitionData(int agentId, out IEnumerable<StateCastVariableDefinitionData> dataSet);
+    }
+}
--- CSharp/SchedulingClients/ServiceOperationResultFactory.cs
+++ CSharp/SchedulingClients/ServiceOperationResultFactory.cs
@@ -7,7 +7,7 @@ using jobs = SchedulingClients.JobsStateServiceReference;
 //using maps = SchedulingClients.MapServiceReference;
 using job = SchedulingClients.JobStateServiceReference;
 using servicing = SchedulingClients.ServicingServiceReference;
-//using agentStatecast = SchedulingClients.AgentStateCastServiceReference;
+using agentStatecast = SchedulingClients.AgentStatecastServiceReference;
 
 namespace SchedulingClients
 {
@@ -104,17 +104,17 @@ namespace SchedulingClients
                 );
         }
 
-        //		public static ServiceOperationResult FromAgentStatecastServiceCallData(agentStatecast.ServiceCallData serviceCallData)
-        //		{
-        //			Exception serviceException = string.IsNullOrEmpty(serviceCallData.Message) ? null : new Exception(serviceCallData.Message);
+        public static ServiceOperationResult FromAgentStatecastServiceCallData(agentStatecast.ServiceCallData serviceCallData)
+        {
+            Exception serviceException = string.IsNullOrEmpty(serviceCallData.Message) ? null : new Exception(serviceCallData.Message);
 
-        //			return new ServiceOperationResult
-        //				(
-        //					(uint)serviceCallData.ServiceCode,
-        //					serviceCallData.ServiceCode.ToString(),
-        //					serviceException,
-        //					null
-        //				);
-        //		}
+            return new ServiceOperationResult
+                (
+                    (uint)serviceCallData.ServiceCode,
+                    serviceCallData.ServiceCode.ToString(),
+                    serviceException,
+                    null
+                );
+        }
     }
 }
