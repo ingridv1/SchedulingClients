7840d5cc3923d5cd2b9e09a338574344c0420ab3
Martin Davies <martin.davies@guidanceautomation.com>
2018-08-17 18:36:58 +0100

    Adds task state client
    All building in any cpu

--- CSharp/SchedulingClients/Client Interfaces/ITaskStateClient.cs
+++ CSharp/SchedulingClients/Client Interfaces/ITaskStateClient.cs
@@ -1,15 +1,11 @@
-﻿//using GAClients;
-//using SchedulingClients.TaskStateServiceReference;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using BaseClients;
+using SchedulingClients.TaskStateServiceReference;
+using System;
 
-//namespace SchedulingClients
-//{
-//    public interface ITaskStateClient : ICallbackClient
-//    {
-//        event Action<TaskProgressData> TaskProgressUpdated;
-//    }
-//}
+namespace SchedulingClients
+{
+    public interface ITaskStateClient : ICallbackClient
+    {
+        event Action<TaskProgressData> TaskProgressUpdated;
+    }
+}
--- CSharp/SchedulingClients/TaskStateServiceCallback.cs
+++ CSharp/SchedulingClients/TaskStateServiceCallback.cs
@@ -1,31 +1,31 @@
-﻿//using SchedulingClients.TaskStateServiceReference;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using SchedulingClients.TaskStateServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace SchedulingClients
-//{
-//    public class TaskStateServiceCallback : ITaskStateServiceCallback
-//    {
-//        public TaskStateServiceCallback()
-//        {
-//        }
+namespace SchedulingClients
+{
+    public class TaskStateServiceCallback : ITaskStateServiceCallback
+    {
+        public TaskStateServiceCallback()
+        {
+        }
 
-//        public event Action<TaskProgressData> TaskProgressUpdated;
+        public event Action<TaskProgressData> TaskProgressUpdated;
 
-//        public void OnCallback(TaskProgressData taskProgressData)
-//        {
-//            Action<TaskProgressData> handlers = TaskProgressUpdated;
+        public void OnCallback(TaskProgressData taskProgressData)
+        {
+            Action<TaskProgressData> handlers = TaskProgressUpdated;
 
-//            if (handlers != null)
-//            {
-//                foreach (Action<TaskProgressData> handler in handlers.GetInvocationList())
-//                {
-//                    handler.BeginInvoke(taskProgressData, null, null);
-//                }
-//            }
-//        }
-//    }
-//}
+            if (handlers != null)
+            {
+                foreach (Action<TaskProgressData> handler in handlers.GetInvocationList())
+                {
+                    handler.BeginInvoke(taskProgressData, null, null);
+                }
+            }
+        }
+    }
+}
