ea6fdfcd4357887e07a88df9f3f54952ea1e57f8
Martin Davies <martin.davies@guidanceautomation.com>
2018-08-17 16:38:09 +0100

    Gets the agent and job builder clients working
    Using base clients

---
+++ CSharp/SchedulingClients/AgentClient.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ServiceModel;
+using SchedulingClients.AgentServiceReference;
+using System.Collections.Generic;
+using BaseClients;
+using NLog;
+
+namespace SchedulingClients
+{
+    internal class AgentClient : AbstractClient<IAgentService>, IAgentClient
+    {
+        private bool isDisposed = false;
+
+        /// <summary>
+        /// Creates a new agent client
+        /// </summary>
+        /// <param name="netTcpUri">net .tcp address of the agent client service</param>
+        public AgentClient(Uri netTcpUri)
+            : base(netTcpUri)
+        {
+        }
+
+        /// <summary>
+        /// Gets all available data on registered agents
+        /// </summary>
+        /// <param name="agentDatas"></param>
+        /// <returns>ServiceOperationResult</returns>
+        public ServiceOperationResult TryGetAllAgentData(out IEnumerable<AgentData> agentDatas)
+        {
+            Logger.Info("TryGetAllAgentData()");
+
+            try
+            {
+                var result = GetAllAgentData();
+                agentDatas = result.Item1;
+                return ServiceOperationResultFactory.FromAgentServiceCallData(result.Item2);
+            }
+            catch (Exception ex)
+            {
+                agentDatas = new AgentData[] { };
+                return HandleClientException(ex);
+            }
+        }
+
+        public ServiceOperationResult TryGetAllAgentsInLifetimeState(out IEnumerable<AgentData> agentDatas, AgentLifetimeState agentLifetimeState)
+        {
+            Logger.Info("");
+
+            try
+            {
+                var result = GetAllAgentsInLifetimeState(agentLifetimeState);
+                agentDatas = result.Item1;
+                return ServiceOperationResultFactory.FromAgentServiceCallData(result.Item2);
+            }
+            catch (Exception ex)
+            {
+                agentDatas = new AgentData[] { };
+                return HandleClientException(ex);
+            }
+        }
+
+        private Tuple<AgentData[], ServiceCallData> GetAllAgentData()
+        {
+            Logger.Debug("GetAllAgentData()");
+
+            if (isDisposed)
+            {
+                throw new ObjectDisposedException("AgentClient");
+            }
+
+            Tuple<AgentData[], ServiceCallData> result;
+
+            using (ChannelFactory<IAgentService> channelFactory = CreateChannelFactory())
+            {
+                IAgentService channel = channelFactory.CreateChannel();
+                result = channel.GetAllAgentData();
+                channelFactory.Close();
+                Logger.Trace("channelFactory closed");
+            }
+
+            return result;
+        }
+
+        private Tuple<AgentData[], ServiceCallData> GetAllAgentsInLifetimeState(AgentLifetimeState agentLifetimeState)
+        {
+            Logger.Debug("GetAllAgentsInLifetimeState()");
+
+            if (isDisposed)
+            {
+                throw new ObjectDisposedException("AgentClient");
+            }
+
+            Tuple<AgentData[], ServiceCallData> result;
+
+            using (ChannelFactory<IAgentService> channelFactory = CreateChannelFactory())
+            {
+                IAgentService channel = channelFactory.CreateChannel();
+                result = channel.GetAllAgentsInLifetimeState(agentLifetimeState);
+                channelFactory.Close();
+                Logger.Trace("channelFactory closed");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CSharp/SchedulingClients/Client Interfaces/IAgentClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SchedulingClients.AgentServiceReference;
+using BaseClients;
+
+namespace SchedulingClients
+{
+    public interface IAgentClient : IClient
+	{
+		ServiceOperationResult TryGetAllAgentData(out IEnumerable<AgentData> agentDatas);
+
+		ServiceOperationResult TryGetAllAgentsInLifetimeState(out IEnumerable<AgentData> agentDatas, AgentLifetimeState agentLifetimeState);
+	}
+}
