2875f92f44d2b93249bef2e250907a9855ef40e8
Martin Davies <martin.davies@guidanceautomation.com>
2018-08-20 14:10:04 +0100

    Updates job builder
    Adds unit test

---
+++ CSharp/SchedulingClients.Test/TJobBuilderClient.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using SchedulingClients.JobBuilderServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BaseClients;
+using System.Threading.Tasks;
+
+namespace SchedulingClients.Test
+{
+    [TestFixture]
+    [Category("JobBuilder")]
+    public class TJobBuilderClient : AbstractClientTest
+    {
+        [OneTimeSetUp]
+        public override void OneTimeSetup()
+        {
+            base.OneTimeSetup();
+
+            IMapClient mapClient = ClientFactory.CreateTcpMapClient(settings);
+            clients.Add(mapClient);
+
+            IJobBuilderClient jobBuilderClient = ClientFactory.CreateTcpJobBuilderClient(settings);
+            clients.Add(jobBuilderClient);  
+        }
+
+        IJobBuilderClient JobBuilderClient => clients.First(e => e is IJobBuilderClient) as IJobBuilderClient;
+
+        IMapClient MapClient => clients.First(e => e is IMapClient) as IMapClient;
+
+        [Test]
+        public void CreateJob()
+        {
+            JobData jobData = null;
+            ServiceOperationResult result = JobBuilderClient.TryCreateJob(out jobData);
+
+            Assert.IsNotNull(jobData);
+            Assert.IsTrue(result.IsSuccessfull);
+        }
+
+    }
+}
