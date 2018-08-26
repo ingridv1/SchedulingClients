ee039b79c89eec4c3b2923db62a056a8f0491ca4
Martin Davies <martin.davies@guidanceautomation.com>
2018-08-26 23:21:27 +0100

    Adds minor unit tests

---
+++ CSharp/SchedulingClients.Test/TJobStateClient.cs
@@ -0,0 +1,37 @@
+﻿using BaseClients;
+using NUnit.Framework;
+using SchedulingClients.JobStateServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingClients.Test
+{
+    [TestFixture]
+    [Category("JobState")]
+    public class TJobStateClient : AbstractClientTest
+    {
+        [OneTimeSetUp]
+        public override void OneTimeSetup()
+        {
+            base.OneTimeSetup();
+
+            IJobStateClient jobStateClient = ClientFactory.CreateTcpJobStateClient(settings);
+            clients.Add(jobStateClient);
+        }
+
+        IJobStateClient JobStateClient => clients.First(e => e is IJobStateClient) as IJobStateClient;
+
+        [Test]
+        public void AssertZero()
+        {
+            JobSummaryData jobSummaryData;
+            ServiceOperationResult result = JobStateClient.TryGetJobSummary(-1, out jobSummaryData);
+
+            Assert.IsFalse(result.IsSuccessfull);
+        }
+
+    }
+}
