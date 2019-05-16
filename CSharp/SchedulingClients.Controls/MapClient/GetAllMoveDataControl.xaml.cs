d2d582620dc47298eb825a45b2bff7a8d479947a
Martin Davies <martin.davies@guidanceautomation.com>
2019-05-16 15:56:39 +0100

    Adds get move data control

---
+++ CSharp/SchedulingClients.Controls/MapClient/GetAllMoveDataControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using BaseClients;
+using SchedulingClients.MapServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchedulingClients.Controls.MapClient
+{
+    public partial class GetAllMoveDataControl : UserControl
+    {
+        public GetAllMoveDataControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IMapClient client = DataContext as IMapClient;
+
+                IEnumerable<MoveData> moveData;
+                ServiceOperationResult result = client.TryGetAllMoveData(out moveData);
+
+                dataGrid.ItemsSource = moveData;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            refreshButton.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        }
+    }
+}
