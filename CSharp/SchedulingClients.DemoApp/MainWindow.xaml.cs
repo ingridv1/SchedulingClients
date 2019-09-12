3024a547083c04cd8425d9dd30e0c0b30be4ce21
Martin Davies <martin.davies@guidanceautomation.com>
2019-09-12 16:31:01 +0100

    Adds task state client controls framework

---
+++ CSharp/SchedulingClients.DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Markdig;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using SchedulingClients.Client_Interfaces;
+using BaseClients;
+
+namespace SchedulingClients.DemoApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			string html = Markdown.ToHtml(Properties.Resources.MainWindowDescription);
+			webBrowser.NavigateToString(html);
+		}
+
+		private void TscControlButton_Click(object sender, RoutedEventArgs e)
+		{
+			IPAddress ipAddress = ipV4Control.ToIPAddress();
+
+			if (ipAddress == null)
+			{
+				MessageBox.Show("IPv4 Address is invalid", "Invalid IP Address", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+
+			using (ITaskStateClient client = SchedulingClients.ClientFactory.CreateTcpTaskStatecastClient(new EndpointSettings(ipAddress)))
+			{
+				TaskStateClientControlsWindow window = new TaskStateClientControlsWindow()
+				{
+					DataContext = client
+				};
+
+				window.ShowDialog();
+			}
+		}
+	}
+}
