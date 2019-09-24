0709a1d0758f6d14bf55a8758aa892835647f82c
Martin Davies <martin.davies@guidanceautomation.com>
2019-09-24 12:40:21 +0100

    Bounding box control

---
+++ CSharp/SchedulingClients.Controls/MapClient/BoundingBoxControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SchedulingClients.Controls.MapClient
+{
+	public partial class BoundingBoxControl : UserControl
+	{
+		public BoundingBoxControl()
+		{
+			InitializeComponent();
+		}
+
+		public Rect ToRect()
+		{
+			Point min = new Point(xRange.Minimum, yRange.Minimum);
+			Point max = new Point(xRange.Maximum, yRange.Maximum);
+
+			return new Rect(min, max);
+		}
+	}
+}
