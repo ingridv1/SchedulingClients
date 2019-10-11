be7950b6aa6ecffb219cf61135b35a1ab33fed90
Martin Davies <martin.davies@guidanceautomation.com>
2019-10-11 14:58:25 +0100

    Adds service types converter

---
+++ CSharp/SchedulingClients.Controls/MapClient/Converters.cs
@@ -0,0 +1,25 @@
+﻿using SchedulingClients.MapServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SchedulingClients.Controls.MapClient
+{
+	public class HasServicesConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			ServiceType[] serviceTypes = value as ServiceType[];
+			return serviceTypes.Any();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
