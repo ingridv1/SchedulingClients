7203a445c22bb4d9f98d9b6b621cf90f32be645a
Martin Davies <martin.davies@guidanceautomation.com>
2019-10-11 15:18:57 +0100

    Renders service types

--- CSharp/SchedulingClients.Controls/MapClient/Converters.cs
+++ CSharp/SchedulingClients.Controls/MapClient/Converters.cs
@@ -6,9 +6,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Text;
 
 namespace SchedulingClients.Controls.MapClient
 {
+	public class SerivcesToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			ServiceType[] serviceTypes = value as ServiceType[];
+
+			if (!serviceTypes.Any()) return "No supported services";
+
+			StringBuilder builder = new StringBuilder();
+
+			int numServices = serviceTypes.Count();
+
+			for (int i = 0; i < numServices; i++)
+			{
+				if (i == numServices -1) builder.Append(serviceTypes[i].ToString());
+				else builder.AppendLine(serviceTypes[i].ToString());
+			}
+
+			return builder.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
 	public class HasServicesConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
