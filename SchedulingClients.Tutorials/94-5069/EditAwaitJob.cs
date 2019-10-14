18fa17b2298032bf56b67d2f1fcd3b2e7345306b
Martin Davies <martin.davies@guidanceautomation.com>
2019-10-14 15:17:23 +0100

    Sorts out folder structure
    Sorts out folder structure and also adds controls for viewing mapitems

---
+++ SchedulingClients.Tutorials/94-5069/EditAwaitJob.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using Moq;
+using SchedulingClients.JobStateServiceReference;
+using SchedulingClients.JobBuilderServiceReference;
+using System.Collections.Generic;
+using GACore.Architecture;
+
+namespace SchedulingClients.Tutorials
+{
+    [TestFixture]
+    [Category("94-5069")]
+    public partial class Examples945069
+    {
+        /// <summary>
+        /// This assumes the job in the BaseScenario example is already present
+        /// 
+        ///     Now we want to edit this further to make the agent drop the load at node 15
+        ///    
+        /// Assumes that kingpin can perform a directive where 
+        ///     Parameter alias "BagV.CoordinatedScenario"
+        ///     A byte value of 131 corresponds to DOCK_LEFT_UNLOAD 
+        /// </summary>
+        [Test]
+        public void EditAwaitJob()
+        {
+            Mock<IJobBuilderClient> moqJobBuilder = new Mock<IJobBuilderClient>();
+            IJobBuilderClient jobBuilder = moqJobBuilder.Object;
+
+            Mock<IJobStateClient> moqJobState = new Mock<IJobStateClient>();
+
+            JobSummaryData jobSummaryMoq = new JobSummaryData()
+            {
+                JobId = 5,
+                JobStatus = JobStatus.InProgress,
+                RootOrderedListTaskId = 8,
+                AssignedAgentId = 9,
+                TaskSummaries = new List<TaskSummaryData>
+                {
+                    new TaskSummaryData()
+                    {
+                        TaskId = 8,
+                        ParentTaskId = null,
+                        TaskStatus = TaskStatus.InProgress,
+                        TaskType = TaskType.OrderedList,
+                        NodeTaskSummaryData = new NodeTaskSummaryData() {NodeId = -1 } // -1 to indicate invalid for a list task
+                    },
+                    new TaskSummaryData() // Pick task
+                    {
+                        TaskId = 9,
+                        ParentTaskId = 8,
+                        TaskStatus = TaskStatus.Completed,
+                        TaskType = TaskType.ServiceAtNode,
+                        NodeTaskSummaryData = new NodeTaskSummaryData() {NodeId = 6 }
+                    },
+                    new TaskSummaryData() // Drop task
+                    {
+                        TaskId = 10,
+                        ParentTaskId = 8,
+                        TaskStatus = TaskStatus.InProgress,
+                        TaskType = TaskType.AwaitAtNode,
+                        NodeTaskSummaryData = new NodeTaskSummaryData() {NodeId = 10}
+                    }
+                }.ToArray()
+            };
+
+            moqJobState.Setup(e => e.TryGetJobSummary(5, out jobSummaryMoq));
+            IJobStateClient jobState = moqJobState.Object;
+
+            //////////////////
+            // Actual Calls //
+            //////////////////
+
+            JobSummaryData jobSummary;
+            jobState.TryGetJobSummary(5, out jobSummary);
+
+            // Determine from the job summary which part of the job you wish to edit.
+
+            if (jobBuilder.TryBeginEditingJob(5).IsSuccessfull)
+            {
+                if (jobBuilder.TryBeginEditingTask(8).IsSuccessfull)
+                {
+                    // We can edit the job -- add a service Task
+                    int serviceTaskId;
+                    jobBuilder.TryCreateServicingTask(jobSummary.RootOrderedListTaskId, 15, ServiceType.Execution, out serviceTaskId);
+
+                    // Add a directive
+                    jobBuilder.TryIssueDirective(serviceTaskId, "BagV.CoordinatedScenario", (byte)131); 
+
+                    // Finish editing
+                    jobBuilder.TryFinishEditingTask(8);
+                    jobBuilder.TryFinishEditingJob(jobSummary.JobId);
+                }
+            }
+        }
+    }
+}
---
+++ SchedulingClients.Tutorials/94-5069/LostPayload.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using Moq;
+using SchedulingClients.AgentAttentionServiceReference;
+using System.Threading;
+using SchedulingClients.JobStateServiceReference;
+using GACore.Architecture;
+
+namespace SchedulingClients.Tutorials._94_5069
+{
+    [TestFixture]
+    [Category("94-5069")]
+    public partial class Examples945069
+    {
+        /// <summary>
+        /// This describes how to resolves the lost payload scenario
+        /// 
+        ///     The agent attention service and job progress data coupled provide enough information
+        ///     to determine agent 9 is in trouble.
+        ///     
+        ///     Checking if 9 has a payload present reveals it has lost the payload.
+        ///     
+        ///     The job can now be aborted. 
+        /// </summary>
+        [Test]
+        public void LostPayload()
+        {
+            Mock<IAgentAttentionClient> moqAgentAttention = new Mock<IAgentAttentionClient>();
+            IAgentAttentionClient agentAttention = moqAgentAttention.Object;
+
+            Mock<IJobStateClient> moqJobState = new Mock<IJobStateClient>();
+            IJobStateClient jobStateClient = moqJobState.Object;
+
+            Mock<IAgentStatecastClient> moqAgentState = new Mock<IAgentStatecastClient>();
+            IAgentStatecastClient agentStateCastClient = moqAgentState.Object;
+
+            Mock<IJobsStateClient> moqJobsState = new Mock<IJobsStateClient>();
+            IJobsStateClient jobsStateClient = moqJobsState.Object;
+
+            //////////////////
+            // Actual Calls //
+            //////////////////
+
+            AgentAttentionData[] receivedAttentionData = null;
+            ManualResetEvent attentionEventHandled = new ManualResetEvent(false);
+
+            JobProgressData receivedJobProgressData = null;
+            ManualResetEvent progressEventHandled = new ManualResetEvent(false);
+
+            agentAttention.AgentAttentionChange += delegate (AgentAttentionData[] obj)
+            {
+                receivedAttentionData = obj;
+                attentionEventHandled.Set();
+            };
+            
+            jobStateClient.JobProgressUpdated += delegate (JobProgressData jobProgressData)
+            {
+                receivedJobProgressData = jobProgressData;
+                progressEventHandled.Set();
+            };
+
+            TriggerAgentAttention(moqAgentAttention);
+            TriggerJobProgress(moqJobState);
+
+            attentionEventHandled.WaitOne(5000);
+            progressEventHandled.WaitOne(5000);
+            Assert.IsNotNull(receivedAttentionData);
+            Assert.IsNotNull(receivedJobProgressData);
+
+            // So now we have received 2x events:
+            //  Agent 9 requires attention
+            //  Job progress is under fault
+
+            // Do some validation on state -> perform behvaiour
+            if (receivedJobProgressData.JobStatus == JobStatus.Failing)
+            {
+                if (receivedAttentionData.Any(e => e.AgentId == receivedJobProgressData.AssignedAgentId
+                    && e.AttentionType == AttentionType.Comms))
+                {
+                    int payloadValue;
+                    agentStateCastClient.TryGetIntegerStatecastValue(9, "payloadPresent", out payloadValue);
+
+                    if (payloadValue == 3)
+                    {
+                        bool success;
+                        jobsStateClient.TryAbortJob(receivedJobProgressData.JobId, out success);
+                    }
+                }
+            }
+        }
+
+        private void TriggerJobProgress(Mock<IJobStateClient> moqJobState)
+        {
+            moqJobState.Raise(e => e.JobProgressUpdated += null, new JobProgressData()
+            {
+                JobId = 6,
+                JobStatus = JobStatus.InProgress,
+                AssignedAgentId = 9
+            });
+        }
+
+        private void TriggerAgentAttention(Mock<IAgentAttentionClient> moqAgentAttention)
+        {
+            AgentAttentionData[] dataSet = new AgentAttentionData[]
+            {
+                new AgentAttentionData()
+                {
+                    AgentId = 9,
+                    AttentionType = AttentionType.Comms                   
+                }
+            };
+
+            moqAgentAttention.Raise(e => e.AgentAttentionChange += null, dataSet);
+        }
+
+    }
+}
