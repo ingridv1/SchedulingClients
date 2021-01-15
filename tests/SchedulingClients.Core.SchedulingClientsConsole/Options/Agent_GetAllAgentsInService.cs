9e813d1253e93099de2535db2670c211a67e97da
Martin Davies <martin.davies@guidanceautomation.com>
2021-01-15 14:47:50 +0000

    Addind debug console
    Adding a debug console project

---
+++ tests/SchedulingClients.Core.SchedulingClientsConsole/Options/Agent_GetAllAgentsInService.cs
@@ -0,0 +1,36 @@
+﻿using BaseClients.Core;
+using CommandLine;
+using GAAPICommon.Architecture;
+using SchedulingClients.Core.AgentServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingClients.Core.SchedulingClientsConsole.Options
+{
+    [Verb("ais", HelpText = "Get all agents in service")]
+    public class Agent_GetAllAgentsInService : AbstractConsoleOption<IAgentClient>
+    {
+
+        protected override IServiceCallResult HandleExecution(IAgentClient client)
+        {
+            IServiceCallResult<AgentDto[]> result = client.GetAllAgentsInLifetimeState(AgentLifetimeState.InService);
+
+            if (result.IsSuccessful())
+            {
+                foreach(AgentDto agent in result.Value)
+                {
+                    Console.WriteLine($"{agent.Id} - {agent.Alias} ({agent.IPAddress})");
+                }
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/SchedulingClients.Core.SchedulingClientsConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseClients.Core;
+using CommandLine;
+using GAAPICommon.Core;
+using SchedulingClients.Core;
+using SchedulingClients.Core.SchedulingClientsConsole.Options;
+
+namespace SchedulingClients.Core.SchedulingClientsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = @"Scheduling Clients Console";
+
+            EndpointSettings endpointSettings = null;
+
+            Parser.Default.ParseArguments<ConfigureEndpointSettingsOptions>(args)
+                .WithParsed<ConfigureEndpointSettingsOptions>(o =>
+                {
+                    endpointSettings = o.CreateTcpEndpointSettings();
+                }
+                )
+                .WithNotParsed<ConfigureEndpointSettingsOptions>(o =>
+                {
+                    Environment.Exit(-1);
+                });
+
+            IAgentClient agentClient = ClientFactory.CreateTcpAgentClient(endpointSettings);
+            IJobStateClient jobStateClient = ClientFactory.CreateTcpJobStateClient(endpointSettings);            
+
+            while(true)
+            {
+                Console.Write("sc>");
+                args = Console.ReadLine().Split();
+
+                Parser.Default.ParseArguments
+                    <Agent_GetAllAgentsInService,
+                    JobState_GetCurrentJobSummaryForAgentId>
+                    (args)
+                    .MapResult(
+                        (Agent_GetAllAgentsInService opts) => opts.ExecuteOption(agentClient),
+                        (JobState_GetCurrentJobSummaryForAgentId opts) => opts.ExecuteOption(jobStateClient),
+                        errs => ServiceCallResultFactory.FromClientException(new Exception("Operation failed"))
+                        );
+            }
+        }
+    }
+}
