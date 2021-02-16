5eecbbef6e2ed3555e33bfe65982617edcc4c155
Martin Davies <martin.davies@guidanceautomation.com>
2021-02-16 11:21:20 +0000

    Update Program.cs

--- tutorials/Tutorial 02/Program.cs
+++ tutorials/Tutorial 02/Program.cs
@@ -1,11 +1,7 @@
 ﻿using BaseClients.Core;
 using SchedulingClients.Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tutorial_02
 {
